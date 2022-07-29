c6dbb5645d3982d468f9a81d0adac995cc4f94e3
David Serrano <pitgear100@gmail.com>
2022-07-29 18:03:30 -0500

    feat: Asignacion por destructuring y uso de props por defecto

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -2,12 +2,17 @@
 import React from "react";
 
 export default function Saludar(props) {
-
+    // Asignacion por destructuring
+    const { userInfo, saludarFn } = props
+    const { nombre = "Anonimo" } = userInfo
+    //
     console.log(props)
+
+    console.log(userInfo)
 
     return(
         <div>
-           <button onClick={() => props.saludarFn(props.userInfo.nombre)}>
+           <button onClick={() => saludarFn(nombre)}>
             Saludar
             </button>
         </div>
