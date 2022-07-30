2c72772f40837d357b3516598ce5ad1b2324163a
David Serrano <pitgear100@gmail.com>
2022-07-29 19:12:32 -0500

    feat: implementacion de Hook de estado y Hook de efectos

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import { Button , Accordion, Card, Alert } from 'react-bootstrap';
+//import { Button , Accordion, Card, Alert } from 'react-bootstrap';
 import './App.css';
-import { ReactComponent as ReactIcon } from './assets/react.svg';
-import Saludar from './components/Saludar';
+import React, { useEffect, useState } from 'react';
+//import { ReactComponent as ReactIcon } from './assets/react.svg';
+//import Saludar from './components/Saludar';
 
 //uso basico de props
 function App() {
-  
-  const user = {
-    nombre: "David",
-    edad: 21,
-    color: "Azul"
-  }
+  // Hook de efectos
+  const [contar, setContar] = useState(0)
+  useEffect(() => {
+    console.log("Total: " + contar)
+  }, [contar])
 
-  const saludarFn = (nombre, edad) => {
-    console.log("Hola " + nombre + " tiene " + edad + " anos.")
-
-    console.log(`Hola ${nombre}, tiene ${edad} anos. `)
+  // Uso de Hook de estado u de efectos
+  const [stateCar, setStateCar] = useState(false)
+  const encenderApagar = ( ) => {
+    setStateCar(!stateCar)
+    setContar(contar + 1)
+    //setStateCar(prevValue => !prevValue)
   }
+  //
+
+  //PROPS en variables y funciones
+  // const user = {
+  //   nombre: "David",
+  //   edad: 21,
+  //   color: "Azul"
+  // }
+
+  // const saludarFn = (nombre, edad) => {
+  //   console.log("Hola " + nombre + " tiene " + edad + " anos.")
+
+  //   console.log(`Hola ${nombre}, tiene ${edad} anos. `)
+  // }
 
   return (
     <div className="App">
-      <h1>React bootstrap</h1>
-      <Button variant="primary">Click</Button>{' '}
-      <Button variant="primary" size="lg">
-        Block level button
-      </Button>
-      <Alert variant="danger">
-          Peligro!!!
-      </Alert>
-      <ReactIcon />
-
+      <header>
+        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+        <h4>Clicks: {contar}</h4>
+        <button onClick={encenderApagar}>Encender / Apagar</button>
+      </header>
     </div>
   );
 }
