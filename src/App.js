70fa7172e1c1cf4ea893096c8671080e2e0c315b
David Serrano <pitgear100@gmail.com>
2022-07-29 19:33:58 -0500

    feat: implementacion de React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 //import { Button , Accordion, Card, Alert } from 'react-bootstrap';
 import './App.css';
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 //import { ReactComponent as ReactIcon } from './assets/react.svg';
 //import Saludar from './components/Saludar';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { Button } from 'bootstrap';
+import Contacto from './Pages/Contacto';
+import QuienSoy from './Pages/QuienSoy';
 
 //uso basico de props
 function App() {
   // Hook de efectos
-  const [contar, setContar] = useState(0)
-  useEffect(() => {
-    console.log("Total: " + contar)
-  }, [contar])
+  // const [contar, setContar] = useState(0)
+  // useEffect(() => {
+  //   console.log("Total: " + contar)
+  // }, [contar])
 
   // Uso de Hook de estado u de efectos
-  const [stateCar, setStateCar] = useState(false)
-  const encenderApagar = ( ) => {
-    setStateCar(!stateCar)
-    setContar(contar + 1)
+  // const [stateCar, setStateCar] = useState(false)
+  // const encenderApagar = ( ) => {
+  //   setStateCar(!stateCar)
+  //   setContar(contar + 1)
     //setStateCar(prevValue => !prevValue)
-  }
+  //}
   //
 
   //PROPS en variables y funciones
@@ -36,11 +40,33 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado"}</h3>
-        <h4>Clicks: {contar}</h4>
-        <button onClick={encenderApagar}>Encender / Apagar</button>
-      </header>
+      <h1>React bootstrap</h1>
+
+      <Router>
+        <div>
+          <Link to="/">
+            <Button> Home </Button>
+          </Link>
+          <Link to="/contacto">
+            <Button>Contacto</Button>
+          </Link>
+          <Link>
+            <Button to="/quien-soy">Quien Soy</Button>
+          </Link>
+          
+        </div>
+
+
+        <Switch>
+
+          <Route path='/contacto'>
+            <Contacto />
+          </Route>
+          <Route path='/quien-soy'>
+            <QuienSoy />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
